531cb55ae6c77ed17c85eb3ddce10a99377266b5
Jeyko Caicedo <jeyko_caicedo@gap.com>
2023-05-22 14:16:05 -0500

    ignore .next folder

--- tb-front/.next/server/flight-server-css-manifest.js
+++ tb-front/.next/server/flight-server-css-manifest.js
@@ -1,14 +1,4 @@
 self.__RSC_CSS_MANIFEST={
-  "cssImports": {
-    "/Users/jeykocaicedo/Desktop/tb/tb-front/app/layout.tsx": [
-      "/Users/jeykocaicedo/Desktop/tb/tb-front/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
-      "/Users/jeykocaicedo/Desktop/tb/tb-front/app/globals.css"
-    ]
-  },
-  "cssModules": {
-    "/Users/jeykocaicedo/Desktop/tb/tb-front/app/page": [
-      "/Users/jeykocaicedo/Desktop/tb/tb-front/app/globals.css",
-      "/Users/jeykocaicedo/Desktop/tb/tb-front/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
-    ]
-  }
+  "cssImports": {},
+  "cssModules": {}
 }
